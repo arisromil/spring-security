4d8139d596b4b7ee0535ed103f5884fdda54fe08
arisromil <arisromil@yahoo.com>
2024-01-03 09:07:24 -0700

    Create UserController.java
    • Intercepts any incoming request
    • Converts the payload of the request to the internal structure of the data
    • Sends the data to model for any needed further processing
    • Gets processed data from the model, and advances it to the view for rendering

---
+++ src/main/java/com/arisromil/springsecurity/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.arisromil.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class UserController {
+    @GetMapping("/")
+    public String homePage() {
+        return "welcome";
+    }
+
+    @GetMapping("/welcome")
+    public String welcomePage() {
+        return "welcome";
+    }
+
+    @GetMapping("/authenticated")
+    public String AuthenticatedPage() {
+        return "authenticated";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage() {
+        return "redirect:/welcome";
+    }
+}
